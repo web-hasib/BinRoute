372c6e504a4819bf777f9de26ea004fbae6655a9
web-hasib <hasibulislam9284@gmail.com>
2026-03-14 08:52:18 +0600

    smooth scroll implemented

--- components/sections/ServiceOverview.tsx
+++ components/sections/ServiceOverview.tsx
@@ -65,7 +65,7 @@ const ServiceOverview = () => {
   ];
 
   return (
-    <section className="py-24 bg-white overflow-hidden">
+    <section id="services" className="py-24 bg-white overflow-hidden">
       <div className="container">
         {/* Main Header */}
         <SectionHeader
